d048aa0e7038f1694c663b3b654533a272a96012
Christian Badolato <cbad1@umbc.edu>
2024-02-13 05:36:23 -0500

    Updating to current state

---
+++ bash/applyGlobalFilterKeepDNS.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+global_filter="!(tcp.analysis.retransmission || tcp.analysis.ack_lost_segment || tcp.analysis.duplicate_ack) && (ip || ipv6) && !(dhcp || dhcpv6 || icmp || icmpv6)"
+
+pcap_filename=`echo "${1%.pcap}"`
+outfile="${pcap_filename}-filtered-with-DNS.pcap"
+
+tshark -r $1 -Y "${global_filter}" -w $outfile
