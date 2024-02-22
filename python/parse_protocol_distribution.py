cd2de9555e86d0754e77065c7f5c742498bcb6ed
Christian Badolato <cbad1@umbc.edu>
2024-02-21 20:03:30 -0500

    Updating repo

--- python/parse_protocol_distribution.py
+++ python/parse_protocol_distribution.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 
 # We don't worry about packets associated with tcp overhead
 global_filter = "!tcp.segment"
+lan_filter = "(eth.dst.ig == 1 || ((ip.src == 10.0.0.0/8 || ip.src == 172.16.0.0/12 || ip.src == 192.168.0.0/16) && (ip.dst == 10.0.0.0/8 || ip.dst == 172.16.0.0/12 || ip.dst == 192.168.0.0/16)))"
+wan_filter = "(eth.dst.ig == 0 && !((ip.src == 10.0.0.0/8 || ip.src == 172.16.0.0/12 || ip.src == 192.168.0.0/16) && (ip.dst == 10.0.0.0/8 || ip.dst == 172.16.0.0/12 || ip.dst == 192.168.0.0/16)))"
 
 # Named constants for easier indexing
 PACKET_COUNT_INDEX = 0
@@ -17,6 +19,20 @@ TXPACKET_COUNT_INDEX = 2
 TXBYTE_COUNT_INDEX = 3
 RXPACKET_COUNT_INDEX = 4
 RXBYTE_COUNT_INDEX = 5
+
+LAN_PACKET_COUNT_INDEX = 6
+LAN_BYTE_COUNT_INDEX = 7
+LAN_TXPACKET_COUNT_INDEX = 8
+LAN_TXBYTE_COUNT_INDEX = 9
+LAN_RXPACKET_COUNT_INDEX = 10
+LAN_RXBYTE_COUNT_INDEX = 11
+
+WAN_PACKET_COUNT_INDEX = 12
+WAN_BYTE_COUNT_INDEX = 13
+WAN_TXPACKET_COUNT_INDEX = 14
+WAN_TXBYTE_COUNT_INDEX = 15
+WAN_RXPACKET_COUNT_INDEX = 16
+WAN_RXBYTE_COUNT_INDEX = 17
 
 def main(argv):
 
@@ -84,13 +100,13 @@ def main(argv):
     with open(outfile_location, "w", newline='') as outfile: # open the csv
 
         lines_to_write = []
-        header = "File, Layer, Proto, Packets, Bytes, TxPackets, TxBytes, RxPackets, RxBytes\n"
+        header = "File,Layer,Proto,Packets,Bytes,TxPackets,TxBytes,RxPackets,RxBytes,LanPackets,LanBytes,LanTxPackets,LanTxBytes,LanRxPackets,LanRxBytes,WanPackets,WanBytes,WanTxPackets,WanTxBytes,WanRxPackets,WanRxBytes\n"
         lines_to_write.append(header)
 
         # First process the overall dict
         for layer, protocols in master_dict.items():
             for proto, values in protocols.items():
-                line_to_write = f"Overall,{layer},{proto},{values[PACKET_COUNT_INDEX]},{values[BYTE_COUNT_INDEX]},{values[TXPACKET_COUNT_INDEX]},{values[TXBYTE_COUNT_INDEX]},{values[RXPACKET_COUNT_INDEX]},{values[RXBYTE_COUNT_INDEX]}\n"
+                line_to_write = f"Overall,{layer},{proto},{values[PACKET_COUNT_INDEX]},{values[BYTE_COUNT_INDEX]},{values[TXPACKET_COUNT_INDEX]},{values[TXBYTE_COUNT_INDEX]},{values[RXPACKET_COUNT_INDEX]},{values[RXBYTE_COUNT_INDEX]},{values[LAN_PACKET_COUNT_INDEX]},{values[LAN_BYTE_COUNT_INDEX]},{values[LAN_TXPACKET_COUNT_INDEX]},{values[LAN_TXBYTE_COUNT_INDEX]},{values[LAN_RXPACKET_COUNT_INDEX]},{values[LAN_RXBYTE_COUNT_INDEX]},{values[WAN_PACKET_COUNT_INDEX]},{values[WAN_BYTE_COUNT_INDEX]},{values[WAN_TXPACKET_COUNT_INDEX]},{values[WAN_TXBYTE_COUNT_INDEX]},{values[WAN_RXPACKET_COUNT_INDEX]},{values[WAN_RXBYTE_COUNT_INDEX]}\n"
                 lines_to_write.append(line_to_write)
 
         # Add linebreak
@@ -100,7 +116,7 @@ def main(argv):
         for file, data in protocol_data_list.items():
             for layer, protocols in data.items():
                 for proto, values in protocols.items():
-                    line_to_write = f"{file},{layer},{proto},{values[PACKET_COUNT_INDEX]},{values[BYTE_COUNT_INDEX]},{values[TXPACKET_COUNT_INDEX]},{values[TXBYTE_COUNT_INDEX]},{values[RXPACKET_COUNT_INDEX]},{values[RXBYTE_COUNT_INDEX]}\n"
+                    line_to_write = f"{file},{layer},{proto},{values[PACKET_COUNT_INDEX]},{values[BYTE_COUNT_INDEX]},{values[TXPACKET_COUNT_INDEX]},{values[TXBYTE_COUNT_INDEX]},{values[RXPACKET_COUNT_INDEX]},{values[RXBYTE_COUNT_INDEX]},{values[LAN_PACKET_COUNT_INDEX]},{values[LAN_BYTE_COUNT_INDEX]},{values[LAN_TXPACKET_COUNT_INDEX]},{values[LAN_TXBYTE_COUNT_INDEX]},{values[LAN_RXPACKET_COUNT_INDEX]},{values[LAN_RXBYTE_COUNT_INDEX]},{values[WAN_PACKET_COUNT_INDEX]},{values[WAN_BYTE_COUNT_INDEX]},{values[WAN_TXPACKET_COUNT_INDEX]},{values[WAN_TXBYTE_COUNT_INDEX]},{values[WAN_RXPACKET_COUNT_INDEX]},{values[WAN_RXBYTE_COUNT_INDEX]}\n"
                     lines_to_write.append(line_to_write)
             lines_to_write.append(",,,,,,,,\n")
 
@@ -154,6 +170,12 @@ def parse_pcap_for_protos(protocol_dict, pcap_file_location, mac):
     ret_dict["Layer 5"] = dict()
     ret_dict["Layer 7"] = dict()
 
+    all_tokens = []
+    lan_tokens = []
+    wan_tokens = []
+
+    # Total
+
     # Set up filters
     # Start with an "all" filter
     filter_string = f",{global_filter},eth.src == {mac} && {global_filter},eth.dst == {mac} && {global_filter}"
@@ -198,28 +220,146 @@ def parse_pcap_for_protos(protocol_dict, pcap_file_location, mac):
         tokens = lines[0].split("|")
         
         # Cut out the empty strings and the interval
-        tokens = [x for x in tokens if x and not "<>" in x]
-
-        # Now we can process the data, data will be in order of protocol, with 6 columns per proto
-        index = 0
-        ret_dict["Total"]["Total"] = [tokens[index], tokens[index + 1], tokens[index + 2], tokens[index + 3], tokens[index + 4], tokens[index + 5]]
-        index += 6
-
-        for layer, data in protocol_dict.items():
-            for proto in data:
-
-                packets = tokens[index]
-                bytes = tokens[index + 1]
-                txpackets = tokens[index + 2]
-                txbytes = tokens[index + 3]
-                rxpackets = tokens[index + 4]
-                rxbytes = tokens[index + 5]
-                ret_dict[layer][proto] = [packets, bytes, txpackets, txbytes, rxpackets, rxbytes]
-                index += 6
-
+        all_tokens = [x for x in tokens if x and not "<>" in x]
 
     else:
         print(f"ERROR: Cannot process {pcap_file_location} - {command.stderr}")
+
+
+    # LAN
+
+    # Set up filters
+    # Start with an "all" filter
+    filter_string = f",{global_filter},eth.src == {mac} && {global_filter},eth.dst == {mac} && {global_filter} && {lan_filter}"
+    for data in protocol_dict.values():
+        for proto in data:
+
+            # Each proto needs 3 search strings, total, tx, rx
+            # Need to account for numerical protocols and non-directly searchable protos
+            if proto.isnumeric():
+                filter_string += f",tcp.port == {int(proto)} || udp.port == {int(proto)} && {global_filter} && {lan_filter}"
+                filter_string += f",(tcp.port == {int(proto)} || udp.port == {int(proto)}) && {global_filter} && {lan_filter} && eth.src == {mac}"
+                filter_string += f",(tcp.port == {int(proto)} || udp.port == {int(proto)}) && {global_filter} && {lan_filter} && eth.dst == {mac}"
+            elif proto == "https":
+                filter_string += f",tcp.port == 443 && {global_filter} && {lan_filter}"
+                filter_string += f",tcp.port == 443 && {global_filter} && {lan_filter} && eth.src == {mac}"
+                filter_string += f",tcp.port == 443 && {global_filter} && {lan_filter} && eth.dst == {mac}"
+            elif proto == "secure-mqtt":
+                filter_string += f",tcp.port == 8883 && {global_filter} && {lan_filter}"
+                filter_string += f",tcp.port == 8883 && {global_filter} && {lan_filter} && eth.src == {mac}"
+                filter_string += f",tcp.port == 8883 && {global_filter} && {lan_filter} && eth.dst == {mac}"
+            elif proto == "http": # Sometimes packets will be sent over HTTP ports without being a true HTTP packet, we count these
+                filter_string += f",(http || tcp.port == 80 || udp.port == 80) && {global_filter} && {lan_filter}"
+                filter_string += f",(http || tcp.port == 80 || udp.port == 80) && {global_filter} && {lan_filter} && eth.src == {mac}"
+                filter_string += f",(http || tcp.port == 80 || udp.port == 80) && {global_filter} && {lan_filter} && eth.dst == {mac}"
+            else:
+                filter_string += f",{proto} && {global_filter} && {lan_filter}"
+                filter_string += f",{proto} && {global_filter} && {lan_filter} && eth.src == {mac}"
+                filter_string += f",{proto} && {global_filter} && {lan_filter} && eth.dst == {mac}"
+
+    # Run the tshark command
+    tshark_command = ["tshark", "-qr", pcap_file_location, "-z", f"io,stat,0{filter_string}"]
+    command = subprocess.run(tshark_command, capture_output=True, text=True)
+    
+    # Check if the command was successful
+    if(command.returncode == 0):  
+    
+        parsed_output = command.stdout
+        lines = parsed_output.split('\n') 
+        
+        # We only care about the one data line that has <>
+        lines = [x for x in lines if "<>" in x]
+        tokens = lines[0].split("|")
+        
+        # Cut out the empty strings and the interval
+        lan_tokens = [x for x in tokens if x and not "<>" in x]
+
+    else:
+        print(f"ERROR: Cannot process {pcap_file_location} - {command.stderr}")
+
+    # WAN
+
+    # Set up filters
+    # Start with an "all" filter
+    filter_string = f",{global_filter},eth.src == {mac} && {global_filter},eth.dst == {mac} && {global_filter} && {wan_filter}"
+    for data in protocol_dict.values():
+        for proto in data:
+
+            # Each proto needs 3 search strings, total, tx, rx
+            # Need to account for numerical protocols and non-directly searchable protos
+            if proto.isnumeric():
+                filter_string += f",tcp.port == {int(proto)} || udp.port == {int(proto)} && {global_filter} && {wan_filter}"
+                filter_string += f",(tcp.port == {int(proto)} || udp.port == {int(proto)}) && {global_filter} && {wan_filter} && eth.src == {mac}"
+                filter_string += f",(tcp.port == {int(proto)} || udp.port == {int(proto)}) && {global_filter} && {wan_filter} && eth.dst == {mac}"
+            elif proto == "https":
+                filter_string += f",tcp.port == 443 && {global_filter} && {wan_filter}"
+                filter_string += f",tcp.port == 443 && {global_filter} && {wan_filter} && eth.src == {mac}"
+                filter_string += f",tcp.port == 443 && {global_filter} && {wan_filter} && eth.dst == {mac}"
+            elif proto == "secure-mqtt":
+                filter_string += f",tcp.port == 8883 && {global_filter} && {wan_filter}"
+                filter_string += f",tcp.port == 8883 && {global_filter} && {wan_filter} && eth.src == {mac}"
+                filter_string += f",tcp.port == 8883 && {global_filter} && {wan_filter} && eth.dst == {mac}"
+            elif proto == "http": # Sometimes packets will be sent over HTTP ports without being a true HTTP packet, we count these
+                filter_string += f",(http || tcp.port == 80 || udp.port == 80) && {global_filter} && {wan_filter}"
+                filter_string += f",(http || tcp.port == 80 || udp.port == 80) && {global_filter} && {wan_filter} && eth.src == {mac}"
+                filter_string += f",(http || tcp.port == 80 || udp.port == 80) && {global_filter} && {wan_filter} && eth.dst == {mac}"
+            else:
+                filter_string += f",{proto} && {global_filter} && {wan_filter}"
+                filter_string += f",{proto} && {global_filter} && {wan_filter} && eth.src == {mac}"
+                filter_string += f",{proto} && {global_filter} && {wan_filter} && eth.dst == {mac}"
+
+    # Run the tshark command
+    tshark_command = ["tshark", "-qr", pcap_file_location, "-z", f"io,stat,0{filter_string}"]
+    command = subprocess.run(tshark_command, capture_output=True, text=True)
+    
+    # Check if the command was successful
+    if(command.returncode == 0):  
+    
+        parsed_output = command.stdout
+        lines = parsed_output.split('\n') 
+        
+        # We only care about the one data line that has <>
+        lines = [x for x in lines if "<>" in x]
+        tokens = lines[0].split("|")
+        
+        # Cut out the empty strings and the interval
+        wan_tokens = [x for x in tokens if x and not "<>" in x]
+
+    else:
+        print(f"ERROR: Cannot process {pcap_file_location} - {command.stderr}")
+
+    # Now we can process the data, data will be in order of protocol, with 6 columns per proto
+    index = 0
+    ret_dict["Total"]["Total"] = [all_tokens[index], all_tokens[index + 1], all_tokens[index + 2], all_tokens[index + 3], all_tokens[index + 4], all_tokens[index + 5],
+                                    lan_tokens[index], lan_tokens[index + 1], lan_tokens[index + 2], lan_tokens[index + 3], lan_tokens[index + 4], lan_tokens[index + 5],
+                                    wan_tokens[index], wan_tokens[index + 1], wan_tokens[index + 2], wan_tokens[index + 3], wan_tokens[index + 4], wan_tokens[index + 5]]
+    index += 6
+
+    for layer, data in protocol_dict.items():
+        for proto in data:
+
+            packets = all_tokens[index]
+            bytes = all_tokens[index + 1]
+            txpackets = all_tokens[index + 2]
+            txbytes = all_tokens[index + 3]
+            rxpackets = all_tokens[index + 4]
+            rxbytes = all_tokens[index + 5]
+            lan_packets = lan_tokens[index]
+            lan_bytes = lan_tokens[index + 1]
+            lan_txpackets = lan_tokens[index + 2]
+            lan_txbytes = lan_tokens[index + 3]
+            lan_rxpackets = lan_tokens[index + 4]
+            lan_rxbytes = lan_tokens[index + 5]
+            wan_packets = wan_tokens[index]
+            wan_bytes = wan_tokens[index + 1]
+            wan_txpackets = wan_tokens[index + 2]
+            wan_txbytes = wan_tokens[index + 3]
+            wan_rxpackets = wan_tokens[index + 4]
+            wan_rxbytes = wan_tokens[index + 5]
+            ret_dict[layer][proto] = [packets, bytes, txpackets, txbytes, rxpackets, rxbytes, 
+                                        lan_packets, lan_bytes, lan_txpackets, lan_txbytes, lan_rxpackets, lan_rxbytes,
+                                        wan_packets, wan_bytes, wan_txpackets, wan_txbytes, wan_rxpackets, wan_rxbytes]
+            index += 6
 
     return ret_dict
 
@@ -234,9 +374,25 @@ def add_file_results_to_overall_results(protocol_data, master_dict):
                 master_dict[layer][proto][TXBYTE_COUNT_INDEX] += int(values[TXBYTE_COUNT_INDEX])
                 master_dict[layer][proto][RXPACKET_COUNT_INDEX] += int(values[RXPACKET_COUNT_INDEX])
                 master_dict[layer][proto][RXBYTE_COUNT_INDEX] += int(values[RXBYTE_COUNT_INDEX])
+                master_dict[layer][proto][LAN_PACKET_COUNT_INDEX] += int(values[LAN_PACKET_COUNT_INDEX])
+                master_dict[layer][proto][LAN_BYTE_COUNT_INDEX] += int(values[LAN_BYTE_COUNT_INDEX])
+                master_dict[layer][proto][LAN_TXPACKET_COUNT_INDEX] += int(values[LAN_TXPACKET_COUNT_INDEX])
+                master_dict[layer][proto][LAN_TXBYTE_COUNT_INDEX] += int(values[LAN_TXBYTE_COUNT_INDEX])
+                master_dict[layer][proto][LAN_RXPACKET_COUNT_INDEX] += int(values[LAN_RXPACKET_COUNT_INDEX])
+                master_dict[layer][proto][LAN_RXBYTE_COUNT_INDEX] += int(values[LAN_RXBYTE_COUNT_INDEX])
+                master_dict[layer][proto][WAN_PACKET_COUNT_INDEX] += int(values[WAN_PACKET_COUNT_INDEX])
+                master_dict[layer][proto][WAN_BYTE_COUNT_INDEX] += int(values[WAN_BYTE_COUNT_INDEX])
+                master_dict[layer][proto][WAN_TXPACKET_COUNT_INDEX] += int(values[WAN_TXPACKET_COUNT_INDEX])
+                master_dict[layer][proto][WAN_TXBYTE_COUNT_INDEX] += int(values[WAN_TXBYTE_COUNT_INDEX])
+                master_dict[layer][proto][WAN_RXPACKET_COUNT_INDEX] += int(values[WAN_RXPACKET_COUNT_INDEX])
+                master_dict[layer][proto][WAN_RXBYTE_COUNT_INDEX] += int(values[WAN_RXBYTE_COUNT_INDEX])
             else:
                 master_dict[layer][proto] = [int(values[PACKET_COUNT_INDEX]), int(values[BYTE_COUNT_INDEX]), int(values[TXPACKET_COUNT_INDEX]), 
-                                                int(values[TXBYTE_COUNT_INDEX]), int(values[RXPACKET_COUNT_INDEX]), int(values[RXBYTE_COUNT_INDEX])]
+                                                int(values[TXBYTE_COUNT_INDEX]), int(values[RXPACKET_COUNT_INDEX]), int(values[RXBYTE_COUNT_INDEX]),
+                                                int(values[LAN_PACKET_COUNT_INDEX]), int(values[LAN_BYTE_COUNT_INDEX]), int(values[LAN_TXPACKET_COUNT_INDEX]), 
+                                                int(values[LAN_TXBYTE_COUNT_INDEX]), int(values[LAN_RXPACKET_COUNT_INDEX]), int(values[LAN_RXBYTE_COUNT_INDEX]),
+                                                int(values[WAN_PACKET_COUNT_INDEX]), int(values[WAN_BYTE_COUNT_INDEX]), int(values[WAN_TXPACKET_COUNT_INDEX]), 
+                                                int(values[WAN_TXBYTE_COUNT_INDEX]), int(values[WAN_RXPACKET_COUNT_INDEX]), int(values[WAN_RXBYTE_COUNT_INDEX])]
             
     return master_dict
 
