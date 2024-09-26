7b52edcc4aa06d4568868a99a6154c8fb794ddaa
Christian Badolato <cbad1@umbc.edu>
2024-09-26 01:59:14 -0400

    Bash script updates

--- bash/helpers/applyGlobalFilterKeepDNS.bash
+++ bash/helpers/applyGlobalFilterKeepDNS.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-global_filter="!(tcp.analysis.retransmission || tcp.analysis.ack_lost_segment || tcp.analysis.duplicate_ack) && (ip || ipv6) && !(dhcp || dhcpv6 || icmp || icmpv6 || nbns || igmp)"
+global_filter="!(tcp.analysis.retransmission || tcp.analysis.ack_lost_segment || tcp.analysis.duplicate_ack) && (ip || ipv6) && !(dhcp || dhcpv6 || icmp || icmpv6 || igmp)"
 
 pcap_filename=`echo "${1%.pcap}"`
 outfile="${pcap_filename}-filtered-with-DNS.pcap"
--- bash/helpers/applyLANFilter.bash
+++ bash/helpers/applyLANFilter.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-global_filter="eth.dst.ig == 1 || ((ip.src == 10.0.0.0/8 || ip.src == 172.16.0.0/12 || ip.src == 192.168.0.0/16) && (ip.dst == 10.0.0.0/8 || ip.dst == 172.16.0.0/12 || ip.dst == 192.168.0.0/16))"
+global_filter="eth.dst.ig == 1 || ((ip.src == 10.0.0.0/8 || ip.src == 172.16.0.0/12 || ip.src == 192.168.0.0/16) && (ip.dst == 10.0.0.0/8 || ip.dst == 172.16.0.0/12 || ip.dst == 192.168.0.0/16 || ipv6.dst == ff00::/8 || ipv6.dst == fe80::/10))"
 
 pcap_filename=`echo "${1%.pcap}"`
 outfile="${pcap_filename}-LAN.pcap"
