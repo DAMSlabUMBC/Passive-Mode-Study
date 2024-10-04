c0ad5fb8b3f4522dac83a0d81630785a57f9463a
Christian Badolato <cbad1@umbc.edu>
2024-10-04 03:01:32 -0400

    Getting repo ready for anonymizing

--- paper-submission-snapshot/bash/applyWANFilter.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-global_filter="eth.dst.ig == 0 && !((ip.src == 10.0.0.0/8 || ip.src == 172.16.0.0/12 || ip.src == 192.168.0.0/16) && (ip.dst == 10.0.0.0/8 || ip.dst == 172.16.0.0/12 || ip.dst == 192.168.0.0/16))"
-
-pcap_filename=`echo "${1%.pcap}"`
-outfile="${pcap_filename}-WAN.pcap"
-
-tshark -r $1 -Y "${global_filter}" -w $outfile
